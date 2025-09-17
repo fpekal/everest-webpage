8862308d7613ffe1204bbf238d5473b5d561e329
Filip Pękala <272985@student.pwr.edu.pl>
2025-09-17 13:02:57 +0200

    Add tailwind to the project
    And some things from progress bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: node({
     mode: 'standalone'
   }),
-	server: {port: 47063}
+
+  server: {port: 47063},
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
